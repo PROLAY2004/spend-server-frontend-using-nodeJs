b5e7026464273b48811015517d376d8d424a549d
Prolay Halder <prolayhalder2004@gmail.com>
2026-07-09 13:15:33 +0530

    Feat : login page UI added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../pages/home/Home.jsx';
+import Login from '../pages/login/Login.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
     },
     {
         path: '*',
